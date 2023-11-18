1a08441a2a16cd293e2991c564ad1245c0bf8304
Jose Manuel Gonzalez Trochez <jgonzalez7243@campuscecep.edu.co>
2023-11-18 18:21:52 -0500

    Delete Clientes

---
+++ Pages/Clientes/Delete.cshtml.cs
@@ -0,0 +1,41 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using RostrosFelicesWEB.Data;
+using RostrosFelicesWEB.Models;
+
+namespace RostrosFelicesWEB.Pages.Clientes
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly RostrosFelicesContext _context;
+
+        public DeleteModel(RostrosFelicesContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+
+        public Cliente Clientes { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if(id == null || _context.Clientes == null)
+            {
+                return NotFound();
+            }
+
+            var cliente = await _context.Clientes.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (cliente == null)
+            {
+                Clientes = Clientes;
+                _context.Clientes.Remove(Clientes);
+                await _context.SaveChangesAsync();
+            }
+            return RedirectToPage("./Index");
+            
+        }
+    }
+}
